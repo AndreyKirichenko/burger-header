bbd47cfbb7e95e86a8c181441aba9fd7a4c08e0e
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2019-05-23 21:50:11 +0300

    markup is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './styles/index.scss';
 
-import './components/App';
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', function (event) {
+  event.currentTarget.classList.toggle('burger--active');
+  menu.classList.toggle('header__menu--active');
+});
+
